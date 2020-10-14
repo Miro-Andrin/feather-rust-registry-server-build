f9cdd700e2565e94d663f2b685f270230e759e72
Miro Andrin Debrunner <miroad@pop-os.localdomain>
2020-10-14 14:51:19 +0200

    made it work with refinary

--- registry/start.sh
+++ registry/start.sh
@@ -3,8 +3,11 @@
 # This command is run on startup of the server inside the container.
 # This compiles the project, and then runs the binary. 
 
+# Enable citex and semver on the server
+psql $DATABASE_URL -c "CREATE EXTENSION citext;"
+psql $DATABASE_URL -c "CREATE EXTENSION semver;"
+
 cd /usr/src/app
-python3 ./DB_init.py
+refinery migrate -e DATABASE_URL  files -p ./migrations
 cargo build --release  || { echo 'buid failed' ; exit 1; }
 /usr/src/app/target/release/feather-registry
-
